dbc9d3df5c7e58f489444a0970236d8c22460df4
75asa <75asa.me@gmail.com>
2022-08-05 20:57:16 +0900

    feat: 🎸 rename

--- packages/web-app/README.md
+++ packages/web-app/README.md
@@ -1,4 +1,4 @@
-# Client
+# GYAVA@web-app
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.2.
 
--- packages/web-app/src/app/app.component.spec.ts
+++ packages/web-app/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'GYAVA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('GYAVA');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'client app is running!',
+      'GYAVA app is running!',
     );
   });
 });
